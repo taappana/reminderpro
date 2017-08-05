6498a64ccd0e5ca69cb815b3ade4c588cbd6d174
taappana <herranen.jani@gmail.com>
2017-08-05 14:33:12 +0300

    list reminders now render

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './reducers'
+import reducer from './reducers';
+import './index.css';
 
 const store = createStore(reducer);
 
